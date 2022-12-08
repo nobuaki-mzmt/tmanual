130a97a0e0af046ccc43f9755ffb74fc21472930
Nobuaki Mizumoto (水元 惟暁) <nobuaki.mzmt@gmail.com>
2022-12-08 22:59:18 +0900

    Added list of dependencies

---
+++ dependencies.cmd
@@ -0,0 +1,10 @@
+# list of dependencies
+pip install pyautogui
+pip install opencv-python
+pip install numpy
+pip install glob
+pip install re
+pip install keyboard
+pip install pyautogui
+pip install tqdm
+pip install PySimpleGUI
