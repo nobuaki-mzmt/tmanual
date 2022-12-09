333a176437cfade184bcfaadfdc0a5de7981da34
Nobuaki Mizumoto <71577022+nobuaki-mzmt@users.noreply.github.com>
2022-12-09 16:45:23 +0900

    fixed requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+def load_requires_from_file(filepath):
+    with open(filepath) as fp:
+        return [pkg_name.strip() for pkg_name in fp.readlines()]
+
 setup(
     name='tmanual',
     version='0.1',
@@ -7,5 +11,6 @@ setup(
     author='Nobuaki Mizumoto',
     author_email="nobuaki.mzmt@gmail.com",
     license='BSD-3',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=load_requires_from_file("requirements.txt")
 )
